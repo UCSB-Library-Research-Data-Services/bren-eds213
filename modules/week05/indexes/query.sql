eca682b410c359b3a067c8f46c17644cd31fe72d
Greg Janée <gjanee@ucsb.edu>
2024-05-01 10:34:17 -0700

    Remove unnecessary files

--- modules/week05/indexes/query.sql
+++
@@ -1,7 +0,0 @@
-SELECT Nest_ID
-    FROM Bird_nests
-    WHERE Site = 'nome' AND
-          Species = 'ruff' AND
-          Year = 1983 AND
-          Observer = 'cbishop' AND
-          ageMethod = 'float';
