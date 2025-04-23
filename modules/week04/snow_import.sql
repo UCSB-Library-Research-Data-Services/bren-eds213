6a0b158962322472a771f71b93b57584f8276e02
Julien Brun <brunj7@users.noreply.github.com>
2025-04-23 08:57:53 -0700

    matching last year table name

--- modules/week04/snow_import.sql
+++ modules/week04/snow_import.sql
@@ -2,7 +2,7 @@
 -- from the ASDN folder run:
 -- duckdb import_test.duckdb 
 
-CREATE TABLE Snow_surveys (
+CREATE TABLE Snow_cover (
     Site VARCHAR NOT NULL,
     Year INTEGER NOT NULL CHECK (Year BETWEEN 1950 AND 2015),
     Date DATE NOT NULL,
@@ -16,12 +16,12 @@ CREATE TABLE Snow_surveys (
     Notes VARCHAR,
     PRIMARY KEY (Site, Plot, Location, Date)
 );
-COPY Snow_surveys FROM 'snow_survey_fixed.csv' (header TRUE);
+COPY Snow_cover FROM 'snow_survey_fixed.csv' (header TRUE);
 
 -- Adding it to the real database from the database folder this time
 -- duckdb database.db
 
-CREATE TABLE Snow_surveys (
+CREATE TABLE Snow_cover (
     Site VARCHAR NOT NULL,
     Year INTEGER NOT NULL CHECK (Year BETWEEN 1950 AND 2015),
     Date DATE NOT NULL,
@@ -37,9 +37,9 @@ CREATE TABLE Snow_surveys (
     FOREIGN KEY (Site) REFERENCES Site (Code)
 );
 -- Nope
-COPY Snow_surveys FROM 'snow_survey_fixed.csv' (header TRUE);
+COPY Snow_cover FROM 'snow_survey_fixed.csv' (header TRUE);
 -- Yep
-COPY Snow_surveys FROM '../ASDN_csv/snow_survey_fixed.csv' (header TRUE);
+COPY Snow_cover FROM '../ASDN_csv/snow_survey_fixed.csv' (header TRUE);
 
 SELECT * from Personnel WHERE Abbreviation ILIKE '%jz%';
 
