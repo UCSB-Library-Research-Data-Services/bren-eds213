1d765de6e1460390075f3f48295f211a1cd9e055
Julien Brun <brunj7@users.noreply.github.com>
2025-04-23 17:17:17 -0700

    matching new data

--- modules/week04/class-script-04-thu-empty.sql
+++ modules/week04/class-script-04-thu-empty.sql
@@ -37,21 +37,16 @@
 -- Ask 9: So? Would it be time well spent to further look into the meaning of zeros?
 
 
--- We found out that actually at the location `sno05` of the site eaba, 0 means NULL... let's update our Snow_cover table
-
+-- We found out that actually at the plot `brw0` of the site barr, 0 means NULL... let's update our Snow_cover table
 CREATE TABLE Snow_cover_backup AS SELECT * FROM Snow_cover; -- Create a copy of the table to be safe (and not cry a lot)
 
 -- For Recall
 SELECT * FROM Site_avg_snowcover;
 SELECT * FROM Site_avg_snowcover_nozeros;
 -- update the 0 for that site
-UPDATE Snow_cover SET Snow_cover = NULL WHERE Location = 'sno05' AND Snow_cover = 0; 
+UPDATE Snow_cover SET Snow_cover = NULL WHERE Plot = 'brw0' AND Snow_cover = 0; 
 -- Check the update was succesful
-SELECT * FROM Snow_cover WHERE Location = 'sno05';
+SELECT * FROM Snow_cover WHERE Plot = 'brw0';
 -- We should probably recompute the avg, let's check
 SELECT * FROM Site_avg_snowcover;
 -- What just happened!?
-
-
--- Ask 10: Let's move on to inspecting the nests and answering the following question: Which shorebird species makes the most eggs? Oh and I need a table with the common names, just because :)
-
